de48c9e8c262a63ff29b05f23f9a95774ff96a6b
Martin <martinmmbote@gmail.com>
2024-07-23 20:38:42 +0300

    leaflet major functionality added

--- adapta-frontend/src/components/Map.jsx
+++ adapta-frontend/src/components/Map.jsx
@@ -1,15 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../style.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents  } from 'react-leaflet';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
 const Map = () => {
+
+  const [markers, setMarkers] = useState([]);
+  const provider = new OpenStreetMapProvider();
+
+  const MapClickHandler = () => {
+    useMapEvents({
+      click: async (e) => {
+        const { lat, lng } = e.latlng;
+        const results = await provider.search({ query: `${lat}, ${lng}` });
+
+        if (results && results.length > 0) {
+          const locationName = results[0].label;
+          console.log(`Location: ${locationName}`);
+
+          setMarkers([...markers, { lat, lng, locationName }]);
+        } else {
+          console.log(`Coordinates: (${lat}, ${lng}) - No location found`);
+          setMarkers([...markers, { lat, lng, locationName: `Coordinates: (${lat}, ${lng})` }]);
+        }
+      },
+    });
+
+    return null;
+  };
+
+  const handleMarkerClick = (index) => {
+    setMarkers(markers.filter((_, i) => i !== index));
+  };
+
   return (
-    <MapContainer center={[1.2921, 36.8219]} zoom={13}>
+    <MapContainer center={[-0.2456874990072913, 37.63907613354113]} zoom={43}>
         <TileLayer 
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            // url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
         />
+
+        {/* <Marker position={[-0.2456874990072913, 37.63907613354113]}>
+          <Popup>
+            Ocha
+          </Popup>
+        </Marker> */}
+
+        {markers.map((marker, index) => (
+          <Marker 
+            key={index} 
+            position={[marker.lat, marker.lng]}
+            eventHandlers={{
+              click: () => {
+                handleMarkerClick(index);
+              },
+              mouseover: (e) => {
+                e.target.openPopup();
+              },
+              mouseout: (e) => {
+                e.target.closePopup();
+              },
+            }}
+          >
+            <Popup>
+              {marker.locationName}
+            </Popup>
+          </Marker>
+        ))}
+        <MapClickHandler />
     </MapContainer>
   )
 }
