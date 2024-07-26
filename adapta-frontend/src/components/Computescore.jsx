d176c5819c8fe54206940a6c65c7c2a985914a98
Martin <martinmmbote@gmail.com>
2024-07-26 14:06:12 +0300

    select crop dropmenu improved

--- adapta-frontend/src/components/Computescore.jsx
+++ adapta-frontend/src/components/Computescore.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Select from 'react-select';
 
 const Computescore = () => {   
     
@@ -16,9 +17,52 @@ const Computescore = () => {
         console.log(formJson);
       }
 
+    
+    //
+    //Select Componet Code
+    const options = [
+      { value: 'Avocado', label: 'Avocado' },
+      { value: 'Tea', label: 'Tea' },
+      { value: 'Banana', label: 'Banana' }
+    ];
+
+
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleChange = (selectedOption) => {
+        setSelectedOption(selectedOption);
+    };
+
+    const customStyles = {
+        control: (provided, state) => ({
+          ...provided, // inherit the default styles
+          background: 'white', // set the background color of the control
+          width: '12vw',
+          height: '1vw',
+          borderWidth: '0.15vw',
+          borderColor: state.isFocused ? 'blue' : '#F3F3F3', // change the border color based on focus state
+          boxShadow: state.isFocused ? null : null, // remove the default box shadow
+          '&:hover': {
+            borderColor: state.isFocused ? 'blue' : 'grey' // change the border color on hover
+          }
+        }),
+        option: (provided, state) => ({
+          ...provided, // inherit the default styles
+          background: state.isSelected ? 'blue' : 'white', // set the background color based on selection state
+          color: state.isSelected ? 'white' : 'black', // set the text color based on selection state
+          '&:hover': {
+            background: 'lightgrey', // change the background color on hover
+            color: 'black' // change the text color on hover
+          }
+        })
+      };
+    // //
+    // //
+    
+
   return (
     <div>
-        <div className='bg-white w-[15.5vw] text-adapta-blue font-semibold text-[0.8vw] pt-[1.3vw] pb-[0.9vw] pl-[1.3vw] rounded-[0.4vw] border-[0.15vw] border-adapta-grey'>
+        <div className='bg-white w-[17.4vw] text-adapta-blue font-semibold text-[0.8vw] pt-[1.3vw] pb-[0.9vw] pl-[1.3vw] rounded-[0.4vw] border-[0.15vw] border-adapta-grey'>
             <form onSubmit={handleSubmit}>
                 <label>
                     <span className='ml-[0.3vw]'>
@@ -29,6 +73,7 @@ const Computescore = () => {
                 </label>
 
                 <label>
+                    <br />
                     <span className='ml-[0.3vw]'>
                         Enter Planting Period:
                     </span>
@@ -38,7 +83,7 @@ const Computescore = () => {
 
                 <br />
 
-                <label>
+                {/* <label>
                     Select a Crop:
 
                     <br />
@@ -48,7 +93,25 @@ const Computescore = () => {
                         <option value="banana">Tea</option>
                         <option value="orange">Crop</option>
                     </select>
-                </label>
+                </label> */}
+
+                <div>
+                    <label>
+                    Select a Crop:
+
+                    <br />
+
+                        <Select
+                            value={selectedOption}
+                            onChange={handleChange}
+                            options={options}
+                            placeholder="Search Crop"
+                            styles={customStyles}
+                            
+                        />
+                        {/* {selectedOption && <p>You selected: {selectedOption.label}</p>} */}
+                    </label>
+                </div>
 
                 <div className='w-[12vw]'>
                     <div className='flex justify-center'>
--- adapta-frontend/src/components/Protectedarea.jsx
+++ adapta-frontend/src/components/Protectedarea.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Protectedarea = () => {
   return (
     <div>
-        <div className='bg-white w-[15.5vw] text-adapta-blue font-semibold text-[0.8vw] leading-[1vw] pt-[1.3vw] pb-[0.9vw] px-[1vw] rounded-[0.4vw] border-[0.15vw] border-adapta-grey mt-[1vw]'>
+        <div className='bg-white w-[17.4vw] h-[11.3vw] text-adapta-blue font-semibold text-[0.8vw] leading-[1vw] pt-[1.3vw] pb-[0.9vw] px-[1vw] rounded-[0.4vw] border-[0.15vw] border-adapta-grey mt-[1vw]'>
             <p>
                 “Biodiversity regions are geographically defined protected areas that are effectively maintained through legal or other ways to preserve biological diversity as well as natural resources and related cultural resources, such as forests and wildlife sanctuaries.(JUCN 1994)”
             </p>
